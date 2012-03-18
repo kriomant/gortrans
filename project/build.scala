eb9b353766a5e8fedc62a771e3569e62b315a23b
Mikhail Trishchenkov <kriomant@gmail.com>
2012-03-18 20:29:24 +0700

    Automatically use appropriate Google Maps key depending on configuration.

--- project/build.scala
+++ project/build.scala
@@ -3,11 +3,28 @@ import sbt._
 import Keys._
 import AndroidKeys._
 import scala.util.matching.Regex
+import xml.XML
 
 object General {
   val apiLevel = SettingKey[Int]("api-level", "Target Android API level")
   val googleMapsJar = SettingKey[File]("google-maps-jar", "Google Maps JAR path")
 	val androidSupportJar = SettingKey[File]("android-support-jar", "Google Support Library JAR path")
+
+	// Generating custom resources file containing some project settings.
+	val customResourcesPath = TaskKey[File]("custom-resources-path", "Path to custom resources file")
+	val customResources = SettingKey[Seq[(String, String)]]("custom-resources", "Custom resources")
+	val generateCustomResources = TaskKey[File]("generate-custom-resources",
+		"Generate resources file based on build settings"
+	)
+	def generateCustomResourcesTask(path: File, res: Seq[(String, String)], streams: TaskStreams) = {
+			val xml =
+				<resources>
+	{res map (r => <string name={r._1}>{r._2}</string>)}
+</resources>
+			XML.save(path.toString, xml)
+			streams.log.info("Generated %s" format path.toString)
+			path
+	}
 
   val settings = Defaults.defaultSettings ++ Seq (
     name := "GorTrans",
@@ -51,7 +68,15 @@ object General {
 	    androidSupportJar <<= (sdkPath in Android) { path =>
 		    (path / "extras" / "android" / "support" / "v4" / "android-support-v4.jar")
 	    },
-	    unmanagedJars in Compile <+= androidSupportJar map { jar => Attributed.blank(jar) }
+	    unmanagedJars in Compile <+= androidSupportJar map { jar => Attributed.blank(jar) },
+
+      customResourcesPath in Android <<= (mainResPath in Android) map { _ / "values" / "generated.xml" },
+      customResources in Android := Seq(),
+      generateCustomResources in (Android, packageDebug) <<= (customResourcesPath in Android, customResources in (Android, packageDebug), streams) map generateCustomResourcesTask,
+	    generateCustomResources in (Android, packageRelease) <<= (customResourcesPath in Android, customResources in (Android, packageRelease), streams) map generateCustomResourcesTask,
+
+      packageDebug in Android <<= (packageDebug in Android) dependsOn (generateCustomResources in (Android, packageDebug)),
+	    packageRelease in Android <<= (packageRelease in Android) dependsOn (generateCustomResources in (Android, packageRelease))
     )
 }
 
