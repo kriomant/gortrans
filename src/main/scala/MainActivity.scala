0aac0da9bf2f0211561e3cafab1f986a1c514975
Mikhail Trishchenkov <kriomant@gmail.com>
2012-03-19 23:19:47 +0700

    Use tabs for different vehicle types in routes list.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -2,59 +2,84 @@ package net.kriomant.gortrans
 
 import _root_.android.os.Bundle
 
-import android.app.ListActivity
 import scala.collection.JavaConverters._
 import android.view.View
-import net.kriomant.gortrans.core.{Route, VehicleType}
-import android.widget.{ListView, SimpleAdapter}
+import net.kriomant.gortrans.core.VehicleType
 import android.content.Intent
+import android.app.TabActivity
+import android.widget.{AdapterView, TabHost, ListView, SimpleAdapter}
+import android.widget.AdapterView.OnItemClickListener
 
-class MainActivity extends ListActivity with TypedActivity {
+class MainActivity extends TabActivity with TypedActivity {
 	private[this] final val TAG = "MainActivity"
-
-	private[this] var routesList: Seq[Route] = null
 
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
 
-	  val dataManager = getApplication.asInstanceOf[CustomApplication].dataManager
-		routesList = dataManager.getRoutesList().values.flatten.toSeq
+	  setContentView(R.layout.routes_list)
+
+	  val tabHost = getTabHost
 
 	  val vehicleTypeNames = Map(
-	    VehicleType.Bus -> R.string.bus,
-	    VehicleType.TrolleyBus -> R.string.trolleybus,
-	    VehicleType.TramWay -> R.string.tramway,
-	    VehicleType.MiniBus -> R.string.minibus
+		  VehicleType.Bus -> R.string.bus,
+		  VehicleType.TrolleyBus -> R.string.trolleybus,
+		  VehicleType.TramWay -> R.string.tramway,
+		  VehicleType.MiniBus -> R.string.minibus
 	  ).mapValues(getString)
 
-		val data = routesList.map { r =>
-			Map(
-				"number" -> getString(R.string.route_name_format,
-					vehicleTypeNames(r.vehicleType),
-					r.name
-				),
-				"description" -> getString(R.string.route_description_format, r.begin, r.end)
-			).asJava
-		}.asJava
+	  val vehicleTypeDrawables = Map(
+		  VehicleType.Bus -> R.drawable.tab_bus,
+			VehicleType.TrolleyBus -> R.drawable.tab_trolleybus,
+			VehicleType.TramWay -> R.drawable.tab_tram,
+			VehicleType.MiniBus -> R.drawable.tab_minibus
+	  ).mapValues(getResources.getDrawable(_))
 
-	  val listAdapter = new SimpleAdapter(
-			this, data,
-			android.R.layout.two_line_list_item,
-			Array("number", "description"),
-			Array(android.R.id.text1, android.R.id.text2)
-		)
+	  val dataManager = getApplication.asInstanceOf[CustomApplication].dataManager
+		val tabs = dataManager.getRoutesList().map { p =>
+			val (vehicleType, routes) = p
 
-	  setListAdapter(listAdapter)
+			val data = routes.map { r =>
+				Map(
+					"number" -> r.name,
+					"begin" -> r.begin,
+					"end" -> r.end
+				).asJava
+			}.asJava
+
+			val factory = new TabHost.TabContentFactory {
+				def createTabContent(p1: String) = {
+					val listAdapter = new SimpleAdapter(
+						MainActivity.this, data,
+						R.layout.routes_list_item,
+						Array("number", "begin", "end"),
+						Array(R.id.route_name, R.id.start_stop_name, R.id.end_stop_name)
+					)
+					val list = new ListView(MainActivity.this)
+					list.setAdapter(listAdapter)
+					list.setOnItemClickListener(new OnItemClickListener {
+						def onItemClick(adapterView: AdapterView[_], view: View, position: Int, id: Long) {
+							val route = routes(position)
+
+							val intent = new Intent(MainActivity.this, classOf[RouteInfoActivity])
+							intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_ID, route.id)
+							intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_NAME, route.name)
+							intent.putExtra(RouteInfoActivity.EXTRA_VEHICLE_TYPE, route.vehicleType.id)
+							startActivity(intent)
+						}
+					})
+
+					list
+				}
+			}
+
+			val tab = tabHost.newTabSpec("routes")
+			tab.setIndicator(vehicleTypeNames(vehicleType), vehicleTypeDrawables(vehicleType))
+			tab.setContent(factory)
+
+			tab
+		}
+	  
+	  tabs.foreach(tabHost.addTab(_))
   }
-
-	override def onListItemClick(l: ListView, v: View, position: Int, id: Long) {
-		val route = routesList(position)
-
-		val intent = new Intent(this, classOf[RouteInfoActivity])
-		intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_ID, route.id)
-		intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_NAME, route.name)
-		intent.putExtra(RouteInfoActivity.EXTRA_VEHICLE_TYPE, route.vehicleType.id)
-		startActivity(intent)
-	}
 
 }
