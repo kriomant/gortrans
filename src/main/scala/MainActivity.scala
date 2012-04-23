288f730299091e416e0fad552f7d3544df4c7970
Mikhail Trishchenkov <kriomant@gmail.com>
2012-04-23 23:12:33 +0700

    Use action bar tabs on routes list screen.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -2,22 +2,25 @@ package net.kriomant.gortrans
 
 import _root_.android.os.Bundle
 
-import android.view.View
 import net.kriomant.gortrans.core.VehicleType
 import android.content.Intent
-import android.app.TabActivity
-import android.widget.AdapterView.OnItemClickListener
 import android.widget._
+import com.actionbarsherlock.app.ActionBar.{Tab, TabListener}
+import android.support.v4.app.{ListFragment, FragmentTransaction}
+import android.view.View
+import com.actionbarsherlock.app.{SherlockFragmentActivity, ActionBar, SherlockActivity}
+import android.content.res.Configuration
 
-class MainActivity extends TabActivity with TypedActivity {
+class MainActivity extends SherlockFragmentActivity with TypedActivity {
 	private[this] final val TAG = "MainActivity"
 
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
 
-	  setContentView(R.layout.routes_list)
-
-	  val tabHost = getTabHost
+	  val actionBar = getSupportActionBar
+	  actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS)
+	  actionBar.setDisplayShowTitleEnabled(false)
+	  actionBar.setDisplayShowHomeEnabled(false)
 
 	  val vehicleTypeNames = Map(
 		  VehicleType.Bus -> R.string.bus,
@@ -34,57 +37,97 @@ class MainActivity extends TabActivity with TypedActivity {
 	  ).mapValues(getResources.getDrawable(_))
 
 	  val dataManager = getApplication.asInstanceOf[CustomApplication].dataManager
+
 		val tabs = dataManager.getRoutesList().map { p =>
 			val (vehicleType, routes) = p
 
-			val factory = new TabHost.TabContentFactory {
-				def createTabContent(p1: String) = {
-					val listAdapter = new ListAdapter with EasyAdapter with SeqAdapter {
-						val context = MainActivity.this
-						val items = routes
-						val itemLayout = R.layout.routes_list_item
-						case class SubViews(number: TextView, begin: TextView, end: TextView)
-						
-						def findSubViews(view: View) = SubViews(
-							view.findViewById(R.id.route_name).asInstanceOf[TextView],
-							view.findViewById(R.id.start_stop_name).asInstanceOf[TextView],
-							view.findViewById(R.id.end_stop_name).asInstanceOf[TextView]
-						)
+			val fragment = new RoutesListFragment(vehicleType)
 
-						def adjustItem(position: Int, views: SubViews) {
-							val route = routes(position)
-							views.number.setText(route.name)
-							views.begin.setText(route.begin)
-							views.end.setText(route.end)
-						}
+			val tab = actionBar.newTab
+				.setIcon(vehicleTypeDrawables(vehicleType))
+				.setTabListener(new TabListener {
+					def onTabSelected(tab: Tab, ft: FragmentTransaction) {
+						ft.replace(android.R.id.content, fragment)
 					}
-
-					val list = new ListView(MainActivity.this)
-					list.setAdapter(listAdapter)
-					list.setOnItemClickListener(new OnItemClickListener {
-						def onItemClick(adapterView: AdapterView[_], view: View, position: Int, id: Long) {
-							val route = routes(position)
-
-							val intent = new Intent(MainActivity.this, classOf[RouteInfoActivity])
-							intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_ID, route.id)
-							intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_NAME, route.name)
-							intent.putExtra(RouteInfoActivity.EXTRA_VEHICLE_TYPE, route.vehicleType.id)
-							startActivity(intent)
-						}
-					})
-
-					list
-				}
-			}
-
-			val tab = tabHost.newTabSpec("routes")
-			tab.setIndicator(vehicleTypeNames(vehicleType), vehicleTypeDrawables(vehicleType))
-			tab.setContent(factory)
+					def onTabReselected(tab: Tab, ft: FragmentTransaction) {}
+					def onTabUnselected(tab: Tab, ft: FragmentTransaction) {}
+				})
 
 			tab
 		}
 	  
-	  tabs.foreach(tabHost.addTab(_))
+	  tabs.foreach(actionBar.addTab(_))
+
+	  if (bundle != null) {
+		  // Restore index of currently selected tab.
+		  actionBar.setSelectedNavigationItem(bundle.getInt("tabIndex"))
+	  }
   }
 
+	override def onSaveInstanceState(outState: Bundle) {
+		// Save index of currently selected tab.
+		outState.putInt("tabIndex", getSupportActionBar.getSelectedNavigationIndex)
+	}
+}
+
+class RoutesListFragment extends ListFragment {
+	var routes: Seq[core.Route] = null
+
+	def this(vehicleType: VehicleType.Value) = {
+		this()
+
+		val args = new Bundle
+		args.putInt("vehicleTypeId", vehicleType.id)
+		setArguments(args)
+	}
+
+	override def onCreate(savedInstanceState: Bundle) {
+		super.onCreate(savedInstanceState)
+
+		val vehicleTypeId = getArguments.getInt("vehicleTypeId")
+
+		val dataManager = getActivity.getApplication.asInstanceOf[CustomApplication].dataManager
+		routes = dataManager.getRoutesList()(VehicleType(vehicleTypeId))
+
+		val listAdapter = new ListAdapter with EasyAdapter with SeqAdapter {
+			val context = getActivity
+			val items = routes
+			val itemLayout = R.layout.routes_list_item
+			case class SubViews(number: TextView, begin: TextView, end: TextView)
+
+			def findSubViews(view: View) = SubViews(
+				view.findViewById(R.id.route_name).asInstanceOf[TextView],
+				view.findViewById(R.id.start_stop_name).asInstanceOf[TextView],
+				view.findViewById(R.id.end_stop_name).asInstanceOf[TextView]
+			)
+
+			def adjustItem(position: Int, views: SubViews) {
+				val route = routes(position)
+				views.number.setText(route.name)
+				views.begin.setText(route.begin)
+				views.end.setText(route.end)
+			}
+		}
+
+		setListAdapter(listAdapter)
+
+		if (savedInstanceState != null) {
+			getListView.onRestoreInstanceState(savedInstanceState.getParcelable("list"))
+		}
+	}
+
+	override def onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+		val route = routes(position)
+
+		val intent = new Intent(getActivity, classOf[RouteInfoActivity])
+		intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_ID, route.id)
+		intent.putExtra(RouteInfoActivity.EXTRA_ROUTE_NAME, route.name)
+		intent.putExtra(RouteInfoActivity.EXTRA_VEHICLE_TYPE, route.vehicleType.id)
+		startActivity(intent)
+	}
+
+	override def onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		outState.putParcelable("list", getListView.onSaveInstanceState())
+	}
 }
