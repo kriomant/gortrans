9d212c3cb22252b6bd4144b8fff036e77d4acd0a
Mikhail Trishchenkov <kriomant@gmail.com>
2012-05-13 16:17:23 +0700

    Add arrivals text pattern.

--- src/main/scala/parsing.scala
+++ src/main/scala/parsing.scala
@@ -211,8 +211,18 @@ object parsing {
 				Right(parseTimes(m.group(1)))
 			},
 
-			("""Отправления с остановки <br />«%s» - <br />""" format Pattern.quote(stopName)).r -> { m: Match =>
+			("""Отправления с остановки <br />«%s» - <br />""" format Pattern.quote(encodedName)).r -> { m: Match =>
 				Right(Seq.empty)
+			},
+
+			("""Отправления с остановки <br />«([^»]+)» - <span class="time">([^<]+)</span><br />От ост. «\1» до ост. «%s» (\d+) мин. пути.<br />""" format Pattern.quote(encodedName)).r -> { m: Match =>
+				val calendar = Calendar.getInstance
+				val increment = m.group(3).toInt
+				Right(parseTimes(m.group(2)) map { time =>
+					calendar.setTime(time)
+					calendar.add(Calendar.MINUTE, increment)
+					calendar.getTime
+				})
 			},
 
 			"""Данные не верны\.""".r -> { m: Match =>
