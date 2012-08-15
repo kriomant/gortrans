c84f563b9e8af363d595a2459bc605748acddd12
Mikhail Trishchenkov <kriomant@gmail.com>
2012-08-15 16:39:01 +0700

    Increase version to 0.1.3.

--- project/build.scala
+++ project/build.scala
@@ -28,8 +28,8 @@ object General {
 
   val settings = Defaults.defaultSettings ++ Seq (
     name := "GorTrans",
-    version := "0.1.2",
-    versionCode := 13,
+    version := "0.1.3",
+    versionCode := 14,
     scalaVersion := "2.8.2",
     apiLevel := 15,
     platformName in Android <<= (apiLevel in Android) { _ formatted "android-%d" }
