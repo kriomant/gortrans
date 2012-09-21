1e22a9de1b2830137dabdf4a67b5f7262324e5fa
Mikhail Trishchenkov <kriomant@gmail.com>
2012-09-22 00:56:08 +0700

    Properly convert empty cursor to iterator.
    Empty cursor's `isLast` method returns `false`, so
     check cursor size additionally.

--- android-app/src/main/scala/CursorIterator.scala
+++ android-app/src/main/scala/CursorIterator.scala
@@ -3,7 +3,8 @@ package net.kriomant.gortrans
 import android.database.Cursor
 
 class CursorIterator[C <: Cursor](cursor: C) extends Iterator[C] {
-	def hasNext: Boolean = !cursor.isLast
+	// If cursor is empty then `isLast` returns `false`, so check size too.
+	def hasNext: Boolean = cursor.getCount > 0 && !cursor.isLast
 
 	def next(): C = {
 		cursor.moveToNext()
