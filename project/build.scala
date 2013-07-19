76921d99fec316f66a81926f7d199c9f91bf1c85
Mikhail Trishchenkov <kriomant@gmail.com>
2013-07-19 11:10:44 +0700

    Increase version to 1.0.4.
    No changes, rebuilt due to corrupted 1.0.3 build.

--- project/build.scala
+++ project/build.scala
@@ -29,8 +29,8 @@ object General {
 
   val settings = Defaults.defaultSettings ++ Seq (
     name := "GorTrans",
-    version := "1.0.3",
-    versionCode := 30,
+    version := "1.0.4",
+    versionCode := 31,
     apiLevel := 15,
     platformName in Android <<= (apiLevel in Android) { _ formatted "android-%d" }
   )
