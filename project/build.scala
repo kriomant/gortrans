37ce8f82eb757bbfc70b127610d07d6008cf043f
artem <art.shaburov@gmail.com>
2020-04-30 20:48:07 +0700

    Small changes in build script

--- project/build.scala
+++ project/build.scala
@@ -63,7 +63,7 @@ object General {
 
       // Use Google Play services SDK.
       googlePlayServices <<= (sdkPath in Android) { path =>
-        path / "extras" / "google" / "google_play_services" / "libproject" / "google-play-services_lib"
+        path / "extras" / "google" / "google_play_services_froyo" / "libproject" / "google-play-services_lib"
       },
       extractApkLibDependencies in Android <+= googlePlayServices map { path =>
         LibraryProject(
