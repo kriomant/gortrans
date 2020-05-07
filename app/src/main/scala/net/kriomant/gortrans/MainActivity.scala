a6bd215128aa093eee788adf33bca5418807ac79
artem <art.shaburov@gmail.com>
2020-05-07 19:57:44 +0700

    Migrate project to Gradle

---
+++ app/src/main/scala/net/kriomant/gortrans/MainActivity.scala
@@ -0,0 +1,140 @@
+package net.kriomant.gortrans
+
+import android.content.{Context, Intent}
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget._
+import com.actionbarsherlock.view.{ActionMode, Menu, MenuItem}
+import net.kriomant.gortrans.core.VehicleType
+
+object MainActivity {
+  val EXTRA_VEHICLE_TYPE = "vehicleType"
+
+  def createIntent(caller: Context, vehicleType: VehicleType.Value = VehicleType.Bus): Intent = {
+    val intent = new Intent(caller, classOf[MainActivity])
+    intent.putExtra(EXTRA_VEHICLE_TYPE, vehicleType.id)
+    intent
+  }
+}
+
+class MainActivity extends MainActivityBase with HavingSidebar {
+  override protected def onDrawerClosed() {
+    if (tabsAreEmbedded) tabsView.setVisibility(View.VISIBLE)
+  }
+
+  override protected def onDrawerOpened() {
+    if (tabsAreEmbedded) tabsView.setVisibility(View.INVISIBLE)
+  }
+}
+
+class MainActivityBase extends RouteListBaseActivity with CreateGroupDialog.Listener {
+
+  import MainActivity._
+
+  private[this] final val TAG = classOf[MainActivityBase].getSimpleName
+  override val layoutResource: Int = R.layout.main_activity
+  var actionModeHelper: MultiListActionModeHelper = _
+
+  override def onCreate(bundle: Bundle) {
+    super.onCreate(bundle)
+
+    val actionBar = getSupportActionBar
+    actionBar.setDisplayHomeAsUpEnabled(true)
+
+    actionModeHelper = new MultiListActionModeHelper(this, ContextActions)
+
+    if (bundle != null) {
+      // Restore index of currently selected tab.
+      setSelectedTab(bundle.getInt("tabIndex"))
+    } else {
+      val vehicleType = VehicleType(getIntent.getIntExtra(EXTRA_VEHICLE_TYPE, VehicleType.Bus.id))
+      setSelectedTab(tabsOrder.indexOf(vehicleType))
+    }
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    super.onCreateOptionsMenu(menu)
+    getSupportMenuInflater.inflate(R.menu.route_list_menu, menu)
+    true
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = item.getItemId match {
+    case R.id.search =>
+      onSearchRequested()
+      true
+
+    case _ => super.onOptionsItemSelected(item)
+  }
+
+  override def onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    // Save index of currently selected tab.
+    outState.putInt("tabIndex", getSelectedTab)
+  }
+
+  /** This method is called by tab fragments when their views are created. */
+  override def registerRoutesList(fragment: RoutesListFragment) {
+    super.registerRoutesList(fragment)
+
+    val listView = fragment.getListView
+    val cursor = fragment.cursor
+
+    listView.setOnItemClickListener(new OnItemClickListener {
+      def onItemClick(parent: AdapterView[_], view: View, position: Int, id: Long) {
+        cursor.moveToPosition(position)
+
+        val intent = RouteInfoActivity.createIntent(MainActivityBase.this, cursor.externalId, cursor.name, cursor.vehicleType)
+        startActivity(intent)
+      }
+    })
+
+    actionModeHelper.attach(listView)
+  }
+
+  def onCreateGroup(dialog: CreateGroupDialog, groupId: Long) {
+    actionModeHelper.finish()
+  }
+
+  object ContextActions extends ActionMode.Callback with ListSelectionActionModeCallback {
+    def onCreateActionMode(mode: ActionMode, menu: Menu): Boolean = {
+      val inflater = mode.getMenuInflater
+      inflater.inflate(R.menu.route_list_actions, menu)
+      true
+    }
+
+    def onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean = {
+      true
+    }
+
+    def onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean = item.getItemId match {
+      case R.id.create_group =>
+        val routeIds = actionModeHelper.getListViews.flatMap(_.getCheckedItemIds)
+        val createGroupDialog = new CreateGroupDialog(routeIds.toSet)
+        createGroupDialog.show(getSupportFragmentManager, "create_group_dialog")
+        true
+      case _ => false
+    }
+
+    def onDestroyActionMode(mode: ActionMode) {}
+
+    def itemCheckedStateChanged(mode: ActionMode) {
+      val count = actionModeHelper.getListViews.map(Compatibility.getCheckedItemCount).sum
+      mode.setTitle(compatibility.plurals.getQuantityString(MainActivityBase.this, R.plurals.routes, count, count))
+    }
+  }
+
+}
+
+class ActionModeCallbackWrapper(callback: ActionMode.Callback) extends ActionMode.Callback {
+  def onCreateActionMode(mode: ActionMode, menu: Menu): Boolean = callback.onCreateActionMode(mode, menu)
+
+  def onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean = callback.onPrepareActionMode(mode, menu)
+
+  def onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean = callback.onActionItemClicked(mode, item)
+
+  def onDestroyActionMode(mode: ActionMode) {
+    callback.onDestroyActionMode(mode)
+  }
+}
+
---
+++ app/src/main/scala/net/kriomant/gortrans/SearchActivity.scala
@@ -0,0 +1,107 @@
+package net.kriomant.gortrans
+
+import android.app.SearchManager
+import android.content.{Context, Intent}
+import android.os.Bundle
+import android.support.v4.widget.CursorAdapter
+import android.text.style.UnderlineSpan
+import android.text.{SpannableString, Spanned}
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.{AdapterView, ImageView, ListView, TextView}
+import com.actionbarsherlock.app.SherlockActivity
+import com.actionbarsherlock.view.MenuItem
+import net.kriomant.gortrans.core.VehicleType
+import net.kriomant.gortrans.utils.closing
+
+class SearchActivity extends SherlockActivity with BaseActivity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+
+    val actionBar = getSupportActionBar
+    actionBar.setDisplayHomeAsUpEnabled(true)
+
+    setContentView(R.layout.search_activity)
+
+    val list = findViewById(R.id.search_result_list).asInstanceOf[ListView]
+    list.setOnItemClickListener(new OnItemClickListener {
+      def onItemClick(parent: AdapterView[_], view: View, position: Int, id: Long) {
+        val cursor = list.getItemAtPosition(position).asInstanceOf[Database.RoutesTable.Cursor]
+        val intent = RouteInfoActivity.createIntent(SearchActivity.this, cursor.externalId, cursor.name, cursor.vehicleType)
+        startActivity(intent)
+      }
+    })
+
+    val intent = getIntent
+    if (intent.getAction == Intent.ACTION_SEARCH) {
+      val query = intent.getStringExtra(SearchManager.QUERY)
+      search(query)
+
+    } else if (intent.getAction == Intent.ACTION_VIEW) {
+      val dbRouteId = intent.getDataString.toLong
+      val db = getApplication.asInstanceOf[CustomApplication].database
+
+      val routeInfoIntent = closing(db.fetchRoute(dbRouteId)) { cursor =>
+        RouteInfoActivity.createIntent(this, cursor.externalId, cursor.name, cursor.vehicleType)
+      }
+
+      startActivity(routeInfoIntent)
+      finish()
+    }
+  }
+
+  def search(query: String) {
+    val database = getApplication.asInstanceOf[CustomApplication].database
+
+    val cursor = database.searchRoutes(query)
+    startManagingCursor(cursor)
+
+    val list = findViewById(R.id.search_result_list).asInstanceOf[ListView]
+    list.setAdapter(new SearchResultAdapter(this, cursor, query))
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = item.getItemId match {
+    case android.R.id.home =>
+      val intent = MainActivity.createIntent(this)
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+      startActivity(intent)
+      true
+
+    case _ => super.onOptionsItemSelected(item)
+  }
+}
+
+class SearchResultAdapter(val context: Context, cursor: Database.RoutesTable.Cursor, query: String)
+  extends CursorAdapter(context, cursor)
+    with EasyCursorAdapter[Database.RoutesTable.Cursor] {
+  val itemLayout: Int = R.layout.search_list_item
+
+  def findSubViews(view: View): SubViews = SubViews(
+    view.findViewById(R.id.vehicle_type_icon).asInstanceOf[ImageView],
+    view.findViewById(R.id.route_name).asInstanceOf[TextView],
+    view.findViewById(R.id.start_stop_name).asInstanceOf[TextView],
+    view.findViewById(R.id.end_stop_name).asInstanceOf[TextView]
+  )
+
+  def adjustItem(cursor: Database.RoutesTable.Cursor, views: SubViews) {
+    views.vehicleType.setImageResource(cursor.vehicleType match {
+      case VehicleType.Bus => R.drawable.tab_bus
+      case VehicleType.TrolleyBus => R.drawable.tab_trolleybus
+      case VehicleType.TramWay => R.drawable.tab_tram
+      case VehicleType.MiniBus => R.drawable.tab_minibus
+    })
+
+    val matchStart = cursor.name.indexOf(query)
+    assert(matchStart != -1)
+    val styledName = new SpannableString(cursor.name)
+    val highlightStyle = new UnderlineSpan
+    styledName.setSpan(highlightStyle, matchStart, matchStart + query.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+
+    views.number.setText(styledName)
+    views.begin.setText(cursor.firstStopName)
+    views.end.setText(cursor.lastStopName)
+  }
+
+  case class SubViews(vehicleType: ImageView, number: TextView, begin: TextView, end: TextView)
+
+}
